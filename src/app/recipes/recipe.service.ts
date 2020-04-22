830f412d150aa8f44bcec4d7b1e59044dfeeb412
unknown <gurpreetsingh05222@gmail.com>
2020-04-22 22:30:32 +0530

    adding ingredient to recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,25 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService{
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Malai Kofta', 'This is first test', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'),
-        new Recipe('Munchurian', 'This is second test', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg')
+        new Recipe(
+            'Malai Kofta', 
+            'This is first test', 
+            'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg', [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe(
+            'Munchurian', 
+            'This is second test', 
+            'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg', [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ])
       ];
 
     getRecipes(){
